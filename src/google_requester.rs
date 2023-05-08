2bc20d2602bb90122be84819de4b3bde91e3dc74
yguenduez <yasin.guenduez1@endress.com>
2023-05-08 13:45:21 +0200

    use ureq instead of reqwest for blocking http(s) requests

--- src/google_requester.rs
+++ src/google_requester.rs
@@ -1,12 +1,10 @@
 pub struct GoogleRequester;
 impl GoogleRequester {
     pub fn fetch(&self) -> String {
-        let body = reqwest::blocking::get("https://www.google.com")
-            .expect("Could not sent request")
-            .text()
-            .expect("Could not transform to text")
-            .to_string();
-        body
+        match ureq::get("https://www.google.com").call() {
+            Ok(response) => response.into_string().expect("Could not transform response into string"),
+            Err(err) => format!("{:?}", err)
+        }
     }
 }
 
